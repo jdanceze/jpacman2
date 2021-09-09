7286c0071dd373bae427cfc2a3622630f1de9a53
Pattarakrit Rattanukul <kaokaoboo@gmail.com>
2021-09-09 18:29:35 +0700

    add BoardTest

---
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -0,0 +1,46 @@
+package nl.tudelft.jpacman.board;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+class BoardTest {
+
+    private final Square[][] grid = {
+        { mock(Square.class)}
+    };
+
+    private final Square[][] grid2 = {
+        {}
+    };
+    //private final Board board = new Board(grid);
+
+    @Test
+    void testInvariant() {
+        Board board = new Board(grid);
+        assertThat(board.invariant()).isTrue();
+    }
+
+    @Test
+    void testWidth() {
+        Board board = new Board(grid);
+        assertThat(board.getWidth()).isEqualTo(1);
+    }
+
+    @Test
+    void testHeight() {
+        Board board = new Board(grid);
+        assertThat(board.getHeight()).isEqualTo(1);
+    }
+
+    @Test
+    void testSquareAt() {
+        Board board = new Board(grid2);
+        assertThat(board.squareAt(0, 0)).isEqualTo(grid2[0][0]);
+    }
+
+
+
+
+}
